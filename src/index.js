15c9278f929724cf7a01bfcffe5e4c52dc33b464
Tanya Kuchma <kuchma.tanya@ukr.net>
2022-01-12 14:56:25 +0200

    axios and async/await

--- src/index.js
+++ src/index.js
@@ -1,1 +1,124 @@
-import './sass/main.scss';
+import photosTpl from './templates/photocard.hbs'
+import './css/styles.css';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.2.min.css';
+import PhotosApiService from './js/photos-service.js';
+
+const ref = {
+    search: document.querySelector(".search-form"),
+    gallery: document.querySelector(".gallery"),
+    loadMoreBtn: document.querySelector(".load-more"),
+}
+
+const photosApiService = new PhotosApiService();
+
+ref.search.addEventListener('submit', onSearch);
+ref.loadMoreBtn.addEventListener('click', onLoadMore);
+
+async function onSearch(evt) {
+    evt.preventDefault();
+    
+    photosApiService.query = evt.currentTarget.elements.searchQuery.value.trim();
+    if (!photosApiService.query) {
+        Notiflix.Notify.failure("Please, enter data.");
+        return;
+    };
+
+    photosApiService.resetPage();
+
+    try {
+        const photos = await photosApiService.fetchPhotos();
+        
+        if (photos.length === 0) {
+            Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+            return;
+        };
+
+        clearPhotosGallery();
+        appendPhotosMarkup(photos);
+        ref.loadMoreBtn.disabled = false;
+        ref.loadMoreBtn.classList.remove('is-hidden');
+
+    } catch(error) {
+        console.log(error);
+    }
+};
+
+async function onLoadMore() {
+    ref.loadMoreBtn.disabled = true;
+    try {
+        const photos = await photosApiService.fetchPhotos();
+        appendPhotosMarkup(photos);
+        ref.loadMoreBtn.disabled = false;
+    } catch(error) {
+        console.log(error);
+    };  
+};
+
+function appendPhotosMarkup(photos) {
+  ref.gallery.insertAdjacentHTML('beforeend', photosTpl(photos));
+}
+
+function clearPhotosGallery() {
+  ref.gallery.innerHTML = '';
+}
+
+
+
+
+// import photosTpl from './templates/photocard.hbs'
+// import './css/styles.css';
+// import Notiflix from 'notiflix';
+// import 'notiflix/dist/notiflix-3.2.2.min.css';
+// import PhotosApiService from './js/photos-service.js';
+
+// const ref = {
+//     search: document.querySelector(".search-form"),
+//     gallery: document.querySelector(".gallery"),
+//     loadMoreBtn: document.querySelector(".load-more"),
+// }
+
+// const photosApiService = new PhotosApiService();
+
+// ref.search.addEventListener('submit', onSearch);
+// ref.loadMoreBtn.addEventListener('click', onLoadMore);
+
+// function onSearch(evt) {
+//     evt.preventDefault();
+    
+//     photosApiService.query = evt.currentTarget.elements.searchQuery.value.trim();
+//     if (!photosApiService.query) {
+//         Notiflix.Notify.failure("Please, enter data.");
+//         return;
+//     };
+
+//     photosApiService.resetPage();
+//     photosApiService.fetchPhotos()
+//         .then(photos => {
+//             if (photos.length === 0) {
+//             Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+//             return;
+//             }  
+            
+//             clearPhotosGallery();
+//             appendPhotosMarkup(photos);
+//             ref.loadMoreBtn.disabled = false;
+//             ref.loadMoreBtn.classList.remove('is-hidden');        
+//         });
+// };
+
+// function onLoadMore() {
+//     ref.loadMoreBtn.disabled = true;
+//     photosApiService.fetchPhotos().then(photos => {
+//         appendPhotosMarkup(photos);
+//         ref.loadMoreBtn.disabled = false;
+//     });
+// };
+
+// function appendPhotosMarkup(photos) {
+//   ref.gallery.insertAdjacentHTML('beforeend', photosTpl(photos));
+// }
+
+// function clearPhotosGallery() {
+//   ref.gallery.innerHTML = '';
+// }
---
+++ src/js/photos-service.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const key = '25186289-71226d48b5f529fb481c1afd8';
+const url = 'https://pixabay.com/api/';
+
+export default class PhotosApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+  
+  async fetchPhotos() {
+    const https = `${url}?key=${key}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
+    try {
+      const responce = await axios.get(https);
+      this.incrementPage();
+      const photos = responce.data.hits
+      return photos;
+    } catch (error) {
+      console.log(error);
+      }
+  } 
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}  
